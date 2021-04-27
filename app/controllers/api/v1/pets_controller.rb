b6eacb5ea157fb319ea498265517f0834e377f64
christopherdent <christopherdent01@gmail.com>
2021-04-26 20:20:10 -0400

    add pets controller

---
+++ app/controllers/api/v1/pets_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::PetsController < ApplicationController
+
+    def index 
+        @pets = Pet.all 
+        render json: @pets 
+
+    end 
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+namespace :api do 
+  namespace :v1 do 
+    resources :pets, only: [:index]  ###building vertically so only index route for right now
+  end
+end
+
+
 end
